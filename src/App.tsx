fbdb4b62ddef89926e1bb1069691aa9ad1082eac
moskalyk <morgan.moskalyk@protonmail.ch>
2023-10-17 14:53:14 -0400

    added contracts and testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import React, { useState } from 'react'
-import { useTheme, Button, Box } from '@0xsequence/design-system'
+import { useTheme, Button, Box, Modal, TextInput, Text, Spinner } from '@0xsequence/design-system'
 import { sequence } from '0xsequence'
+import { AnimatePresence } from 'framer-motion'
+import { ethers, utils } from 'ethers'
+import { abi } from './abi'
 
 let circles: any = []
 let lines: any = []
+let time = 0
+
+const VAULTIMOR_CONTRACT = '0xB9433dfCc18A9BF36e61a78E494D1acA68474e3a'; // <- change this
 
 function App() {
   const [init, setInit] = useState(false)
   const [loggedIn, setLoggedIn] = useState<any>(false)
+  const [isOpen, toggleModal] = useState(false)
+  const [showLoader, setShowLoader] = useState(false)
+  const [contractAddress, setContractAddress] = useState('')
+  const [tokenID, setTokenID] = useState('')
+  const [signature, setSignature] = useState('')
+
   const {setTheme, theme} = useTheme()
 
-  sequence.initWallet({defaultNetwork: 'arbitrum'})
+  sequence.initWallet({defaultNetwork: 'arbitrum-goerli'}) // <- and this
 
   function getRandomPointInSphere(R: number) {
       let theta = Math.random() * Math.PI;
@@ -47,26 +59,106 @@ function App() {
     }
   }
 
+  const getArrayValueAtIndex = async () => {
+    // Connect to a provider
+    let provider = new ethers.providers.JsonRpcProvider("https://nodes.sequence.app/arbitrum-goerli"); // <- and this
+
+    // Create a contract instance
+    const contract = new ethers.Contract(VAULTIMOR_CONTRACT, abi, provider);
+
+    let value = await contract.vaults(0);
+    console.log(value.toString());
+  }
+
   React.useState(() => {
     if(!init){
       setInit(true)
       setTheme('light')
       plotPointsInSVG();
     }
+
+    // Example usage:
+    getArrayValueAtIndex();
   })
+
+  const openModal = () => {
+    toggleModal(true)
+  }
+  
+  const handleSigning = async () => {
+    const wallet = await sequence.getWallet()
+    const signer = await wallet.getSigner(137)
+    // bytes memory data = abi.encodePacked(_vaulter, _contractAddress, _tokenID, _recipient, _time);
+    time = Math.floor(Date.now() / 1000)+100
+    const hash = utils.solidityPack(['address','address', 'uint', 'address', 'uint'], [await wallet.getAddress(), contractAddress, tokenID, await wallet.getAddress(), time])
+    const _signature = await signer.signMessage(hash, {chainId: 137})
+    console.log(_signature)
+    setSignature(_signature)
+  }
+
+  const approve = async () => {
+    const wallet = await sequence.getWallet()
+
+    // ABI for the ERC-721 approve function
+    const erc721Abi = [
+      'function approve(address to, uint256 tokenId) external'
+    ];
+
+    const erc721Interface = new utils.Interface(erc721Abi);
+
+    // Example of encoding the approve function
+    const toAddress = VAULTIMOR_CONTRACT;  // The address you're approving
+    const data = erc721Interface.encodeFunctionData('approve', [toAddress, tokenID]);
+
+    console.log(data);
+
+    const tx = {
+      to: contractAddress,
+      data
+    }
+
+    const ercVaultimorAbi = [
+      'function create(address _vaulter, address _contractAddress, uint _tokenID, address _recipient, uint _time, bytes calldata signature_) onlyMod external'
+    ];
+
+    const vaultimorInterface = new utils.Interface(ercVaultimorAbi);
+    const data1 = vaultimorInterface.encodeFunctionData('create', [await wallet.getAddress(), contractAddress, tokenID, await wallet.getAddress(), time, signature]);
+
+    const tx1 = {
+      to: VAULTIMOR_CONTRACT,
+      data: data1
+    }
+
+    const signer = wallet.getSigner(80001)
+    const res = await signer.sendTransaction([tx, tx1])
+
+    console.log(res)
+
+    toggleModal(false)
+    setSignature('')
+    setTokenID('')
+    setContractAddress('')
+  }
 
   return (
     <div className='App'>
       {
         loggedIn 
         ? 
-        <>
+        <div style={{textAlign: 'center', margin: 'auto'}}>
             <br/>
             <br/>
+            <Box>
+              <Button label="create vault" onClick={openModal}/>
+            </Box>
             <br/>
             <h1 style={{fontFamily: 'Tilt Neon', textAlign: 'center'}}>your vaults</h1>
+            <br/>
             <p style={{fontFamily: 'Tilt Neon', textAlign: 'center'}}>nothing to show</p>
-        </> 
+            <br/>
+            <br/>  
+
+        </div> 
         :  
         <>
           <div style={{textAlign: 'center', margin: 'auto'}}>
@@ -96,7 +188,81 @@ function App() {
           </div>
         </>
       }
+<AnimatePresence>
+        {
 
+          isOpen 
+            && 
+            <Modal  onClose={() => toggleModal(false)}>
+
+                <Box
+                  flexDirection="column"
+                  justifyContent="space-between"
+                  height="full"
+                  padding="16"
+                >
+                    <Box marginTop="5" marginBottom="4">
+                      <br/>
+                      <Text variant="normal" color="text80">
+                        Enter your the contract address and token id you want to vault, then sign the vault and approve our contract to hold the collectible
+                      </Text>
+
+                      <Box marginTop="6">
+                        <TextInput
+                          onChange={(ev: { target: { value: any } }) => {
+                            setContractAddress(ev.target.value)
+                          }}
+                          // ref={inputRef}
+                          // onBlur={() => setEmailWarning(!!email && !isEmailValid)}
+                          // value={email}
+                          placeholder="contract address"
+                          required
+                        />
+                        <br/>
+                        <TextInput
+                          name="email"
+                          type="email"
+                          onChange={(ev: { target: { value: any } }) => {
+                            setTokenID(ev.target.value)
+                          }}
+                          placeholder="token ID"
+                          required
+                        />
+                        {/* {showEmailWarning && (
+                          <Text as="p" variant="small" color="negative" marginY="2">
+                            Invalid email address
+                          </Text>
+                        )} */}
+                      </Box>
+                      <Box gap="2" marginY="4" alignItems="center" justifyContent="center">
+                        {showLoader ? (
+                          <Spinner />
+                        ) : (
+                          <Button
+                            variant="primary"
+                            // width="1/4"
+                            // disabled={!isEmailValid}
+                            label="Sign Vault"
+                            onClick={() => handleSigning()}
+                            // data-id="continueButton"
+                          />
+                        )}
+                        <br/>
+                        <Button
+                            variant="primary"
+                            // width="1/4"
+                            disabled={signature == ''}
+                            label="Approve Contract"
+                            onClick={() => approve()}
+                            // data-id="continueButton"
+                          />
+                      </Box>
+                    </Box>
+                </Box>
+            </Modal>
+        }
+
+          </AnimatePresence>
     </div>
 
   )
---
+++ src/abi.ts
@@ -0,0 +1,156 @@
+const abi = [
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_mod",
+				"type": "address"
+			}
+		],
+		"name": "addToModList",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_vaulter",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_contractAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_tokenID",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "_recipient",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_time",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bytes",
+				"name": "signature_",
+				"type": "bytes"
+			}
+		],
+		"name": "create",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_contractAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_tokenID",
+				"type": "uint256"
+			}
+		],
+		"name": "retrieve",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_contractAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_tokenID",
+				"type": "uint256"
+			}
+		],
+		"name": "unlock",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "mods",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "vaults",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "time",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenID",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "sender",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "recipient",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "contractAddress",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+export {
+    abi
+}
